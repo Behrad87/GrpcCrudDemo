9ce657d5f0f35d920ba3a571dcdd9d8940c15c46
zarei.behrad <zarei.behrad@gandomcs.com>
2026-05-28 13:07:22 +0330

    add grpc reflection packages

--- Program.cs
+++ Program.cs
@@ -1,18 +1,30 @@
 using GrpcCrudDemo.Services;
 using GrpcCrudDemo.Data;
+
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
-builder.Services.AddDbContext<AppDbContext>(opts => opts.UseInMemoryDatabase("persons"));
+builder.Services.AddGrpcReflection();
+
+builder.Services.AddDbContext<AppDbContext>(opts =>
+    opts.UseInMemoryDatabase("persons"));
+
 builder.Services.AddScoped<PersonService>();
 
-var app = builder.Build();
+var app = builder.Build();  
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<PersonService>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapGrpcReflectionService();
+}
+
+app.MapGet("/", () =>
+    "Communication with gRPC endpoints must be made through a gRPC client.");
 
 app.Run();
